3146a60914889dfc31ffc826f3231d1891689737
Brynne Henderson <bwallace7430@gmail.com>
2024-02-24 15:01:26 -0700

    fix file structure

---
+++ shared/src/main/java/model/UserData.java
@@ -0,0 +1,9 @@
+package model;
+
+import com.google.gson.*;
+
+public record UserData(String username, String password, String email) {
+    public String toString(){
+        return new Gson().toJson(this);
+    }
+}
