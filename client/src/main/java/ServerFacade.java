1eda6aa7aeeff9e2748740d4488d2500dbba7828
Brynne Henderson <bwallace7430@gmail.com>
2024-03-19 19:55:16 -0600

    basic repl and server facade

--- client/src/main/java/ServerFacade.java
+++ client/src/main/java/ServerFacade.java
@@ -5,8 +5,8 @@ import java.net.HttpURLConnection;
 import java.net.URI;
 import java.util.*;
 public class ServerFacade {
-    private static void writeRequestBody(Map body, HttpURLConnection http) throws IOException {
-        if (!body.isEmpty()) {
+    private static void writeRequestBody(Map<String, String> body, HttpURLConnection http) throws IOException {
+        if (!(body == null)) {
             http.setDoOutput(true);
             try (var outputStream = http.getOutputStream()) {
                 var jsonBody = new Gson().toJson(body);
@@ -15,47 +15,65 @@ public class ServerFacade {
         }
     }
 
-    private static void writeRequestHeader(String header, HttpURLConnection http) {
-        if (!header.isEmpty()) {
+
+    private static void writeRequestHeader(String authToken, HttpURLConnection http) {
+        if (!(authToken == null)) {
             http.setDoOutput(true);
-            http.addRequestProperty("Authorization", header);
+            http.addRequestProperty("Authorization", authToken);
         }
-        http.addRequestProperty("Content-Type", "application/json");
+        // check for non-json routes
+        http.addRequestProperty("Accept", "application/json");
     }
-    private static void sendRequest(String url, String method, String[] args, String header) throws Exception {
+
+    private static void sendRequest(String url, String method, Map<String, String> body, String authToken) throws Exception {
         URI uri = new URI(url);
         HttpURLConnection http = (HttpURLConnection) uri.toURL().openConnection();
-        var body = Map.of("username", args[0], "password", args[1], "email", args[2]);
 
         http.setReadTimeout(5000);
         http.setRequestMethod(method);
-        writeRequestHeader(header, http);
+        writeRequestHeader(authToken, http);
         writeRequestBody(body, http);
 
         http.connect();
     }
 
-    public static void createUser(String[] body, String header) throws Exception {
-        sendRequest("http://localhost:8080/user", "POST", body, header);
+    private static void sendRequest(String url, String method, Map<String, String> body) throws Exception{
+        sendRequest(url, method, body, null);
     }
 
-    public static void createSession(String[] body, String header) throws Exception {
-        sendRequest("http://localhost:8080/session", "POST", body, header);
+    private static void sendRequest(String url, String method) throws Exception{
+        sendRequest(url, method, null, null);
     }
 
-    public static void deleteSession(String[] body, String header) throws Exception {
-        sendRequest("http://localhost:8080/session", "DELETE", body, header);
+    private static void sendRequest(String url, String method, String authToken) throws Exception{
+        sendRequest(url, method, null, authToken);
     }
 
-    public static void listGame(String[] body, String header) throws Exception {
-        sendRequest("http://localhost:8080/game", "GET", body, header);
+    public static void createUser(String[] args) throws Exception {
+        var body = Map.of("username", args[0], "password", args[1], "email", args[2]);
+        sendRequest("http://localhost:8080/user", "POST", body);
     }
 
-    public static void createGame(String[] body, String header) throws Exception {
-        sendRequest("http://localhost:8080/post", "POST", body, header);
+    public static void createSession(String[] args) throws Exception {
+        var body = Map.of("username", args[0], "password", args[1]);
+        sendRequest("http://localhost:8080/session", "POST", body);
     }
 
-    public static void joinGames(String[] body, String header) throws Exception {
-        sendRequest("http://localhost:8080/put", "PUT", body, header);
+    public static void deleteSession() throws Exception {
+        sendRequest("http://localhost:8080/session", "DELETE");
+    }
+
+    public static void listGame(String authToken) throws Exception {
+        sendRequest("http://localhost:8080/game", "GET", authToken);
+    }
+
+    public static void createGame(String arg, String authToken) throws Exception {
+        var body = Map.of("gameName", arg);
+        sendRequest("http://localhost:8080/post", "POST", body, authToken);
+    }
+
+    public static void joinGames(String[] args, String authToken) throws Exception {
+        var body = Map.of("playerColor", args[0], "gameID", args[1]);
+        sendRequest("http://localhost:8080/put", "PUT", body, authToken);
     }
     }
---
+++ client/src/main/java/ui/Repl.java
@@ -0,0 +1,37 @@
+package ui;
+import java.util.Scanner;
+import static ui.EscapeSequences.*;
+
+public class Repl {
+    //private final PetClient client;
+
+    public Repl(String serverUrl) {
+        //client = new PetClient(serverUrl, this);
+    }
+
+    public void run() {
+        System.out.println("\uD83D\uDC36 Welcome to chess! Type help to begin.");
+        //System.out.print(client.help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                //result = client.eval(line);
+                System.out.print(SET_TEXT_COLOR_BLUE + result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
+
+    private void printPrompt() {
+        System.out.print("\n" + RESET_TEXT_COLOR + ">>> " + SET_TEXT_COLOR_GREEN);
+    }
+
+}
